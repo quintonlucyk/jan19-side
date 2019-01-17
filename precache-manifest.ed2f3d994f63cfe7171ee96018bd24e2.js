45bed4bf48f20f2a9fffa249e4fa02cc5c8b3e19
Quinton Lucyk <qlucyk@uwaterloo.ca>
2019-01-17 11:12:08 -0500

    Updates

---
+++ precache-manifest.ed2f3d994f63cfe7171ee96018bd24e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cb5ba9008863108908b",
+    "url": "/jan19-side/static/js/runtime~main.6cb5ba90.js"
+  },
+  {
+    "revision": "431fa8ee4d2a4cd50347",
+    "url": "/jan19-side/static/js/main.431fa8ee.chunk.js"
+  },
+  {
+    "revision": "a2484651c2c92e76e5e8",
+    "url": "/jan19-side/static/js/1.a2484651.chunk.js"
+  },
+  {
+    "revision": "431fa8ee4d2a4cd50347",
+    "url": "/jan19-side/static/css/main.0c48e1b1.chunk.css"
+  },
+  {
+    "revision": "69c9b4da62524830c414d2428c90088e",
+    "url": "/jan19-side/index.html"
+  }
+];
