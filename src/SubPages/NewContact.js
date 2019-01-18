405ca534ceceea979bbbd53697079eb89dfab305
Quinton Lucyk <qlucyk@uwaterloo.ca>
2019-01-18 11:16:19 -0500

    add contact inputs

--- src/SubPages/NewContact.js
+++ src/SubPages/NewContact.js
@@ -6,6 +6,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 class NewContact extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      name: "",
+      phoneNumber: "",
+      email: "",
+      notes: "",
+    };
     //this.emailKeyUp = this.emailKeyUp.bind(this);
   }
 
@@ -13,9 +19,49 @@ class NewContact extends Component {
     this.props.onCancelContact();
   };
 
+  handleAddition = () => {
+
+  };
+
+  nameKeyUp = (e) => {
+    this.setState({name: e.target.value});
+  };
+
+  emailKeyUp = (e) => {
+    this.setState({email: e.target.value});
+  };
+
+  phoneNumberKeyUp = (e) => {
+    this.setState({phoneNumber: e.target.value});
+  };
+
+  notesKeyUp = (e) => {
+    this.setState({notes: e.target.value});
+  };
+
   render() {
     return (
       <div>
+        <p>NewContact</p>
+        <form onSubmit={this.handleAddition}>
+          <label>
+            Name:
+            <input type="text" name="name" onKeyUp={this.nameKeyUp} />
+          </label>
+          <label>
+            Email:
+            <input type="text" name="email" onKeyUp={this.emailKeyUp} />
+          </label>
+          <label>
+            Phone number:
+            <input type="text" name="phoneNumber" onKeyUp={this.phoneNumberKeyUp} />
+          </label>
+          <label>
+            Notes:
+            <textarea type="text" name="notes" onKeyUp={this.notesKeyUp} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <button onClick={this.cancelNewContact}>Cancel</button>
       </div>
     );
